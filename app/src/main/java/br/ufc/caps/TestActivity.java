f394c8586737fb72746cf4033918d029e69611f4
messiaslima <messiaslima.03@gmail.com>
2016-05-05 10:12:59 -0300

    Initial commit

---
+++ app/src/main/java/br/ufc/caps/TestActivity.java
@@ -0,0 +1,24 @@
+package br.ufc.caps;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Essa Activity foi criada apenas para testar as funcionalidades criadas e não deve ser usada na versão final do app
+ * TODO:Modo Avião;
+ * TODO:Wifi;
+ * TODO:3G;
+ * TODO:Bluetooth;
+ * TODO:Ringtone(Ligação)
+ * TODO:Vibração(Ligação)
+ * TODO:Brilho de Tela
+ * TODO:Wallpaper
+ */
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
