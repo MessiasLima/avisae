f674a8a8cd68833b84814e4f12f0043ef825bbbc
sergiosmarinho <sergio.marinho94@gmail.com>
2016-06-17 15:04:18 -0300

    com ImageButton direitinho e filtros pros pros botes - Ativity new local

--- app/src/main/java/br/ufc/caps/activity/NewLocalActivity.java
+++ app/src/main/java/br/ufc/caps/activity/NewLocalActivity.java
@@ -1,8 +1,10 @@
 package br.ufc.caps.activity;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import br.ufc.caps.R;
 
@@ -14,6 +16,12 @@ public class NewLocalActivity extends AppCompatActivity {
         ActionBar ac = getSupportActionBar();
         ac.setTitle("app");
         setContentView(R.layout.activity_new_local);
+        ImageButton bt2 = (ImageButton) this.findViewById(R.id.botaoIm2);
+        bt2.setColorFilter(Color.argb(150, 255, 255, 255));
+        ImageButton bt3 = (ImageButton) this.findViewById(R.id.botaoIm3);
+        bt3.setColorFilter(Color.argb(200, 255, 255, 255));
+        ImageButton bt4 = (ImageButton) this.findViewById(R.id.botaoIm3);
+        bt4.setColorFilter(Color.argb(200, 255, 255, 255));
     }
 
 }
