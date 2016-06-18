a5122a0465a210eeef4a8166c4472c8508596f44
sergiosmarinho <sergio.marinho94@gmail.com>
2016-06-18 03:09:42 -0300

    com api de escolher lugar - falta testar

--- app/src/main/java/br/ufc/caps/activity/NewLocalActivity.java
+++ app/src/main/java/br/ufc/caps/activity/NewLocalActivity.java
@@ -1,10 +1,19 @@
 package br.ufc.caps.activity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import br.ufc.caps.R;
 
@@ -22,6 +31,29 @@ public class NewLocalActivity extends AppCompatActivity {
         bt3.setColorFilter(Color.argb(200, 255, 255, 255));
         ImageButton bt4 = (ImageButton) this.findViewById(R.id.botaoIm3);
         bt4.setColorFilter(Color.argb(200, 255, 255, 255));
+    }
+
+    public void escolherLugar(View v){
+        Log.e("ei", "veio");
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult(builder.build(this), 1);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
 }
